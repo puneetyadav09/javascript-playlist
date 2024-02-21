3ebe2931407060c426685318c8b953b0782624bd
Puneet <94126652+puneetyadav09@users.noreply.github.com>
2024-02-21 18:45:29 +0000

    commit 02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";   // treat all JS code as newer version
+
+// alert("hello");    // alert is used in browser
+
+let name = "Puneet";   // String
+let age = 20;          // Number
+let isLoggedIn = true; // Boolean
+let a = null;          // null
+let b = undefined      // undefined
+
+/*
+Primitive
+1.String
+2.Number
+3.Boolean
+4.Null
+5.Undefined
+6.Symbol
+7.BigInt  =>  2^53 - 1
+
+
+
+
+Secondary
+1.Object
+
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof "Puneet");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
