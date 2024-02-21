9cd9a6e13b3d34b9a049c65c26ee1333555081c4
Puneet <94126652+puneetyadav09@users.noreply.github.com>
2024-02-21 20:41:41 +0000

    commit 03_conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,96 @@
+// let score = 33
+
+// console.log(typeof score);
+
+// let scoreInString = "'" + score + "'";
+// console.log(typeof scoreInString);
+// console.log(scoreInString);
+
+// let a = "33";
+// let b = "33abc";
+// let c = null;
+// let d = undefined;
+
+// let aInNumber = Number(a);     //convert it in a number with value 33
+// let bInNumber = Number(b);     //convert it in a number with value NaN
+// let cInNumber = Number(c);     //convert it in a number with value 0
+// let dInNumber = Number(d);     //convert it in a number with value NaN
+
+// console.log(typeof aInNumber);
+// console.log(aInNumber);
+// console.log(typeof bInNumber);
+// console.log(bInNumber);
+// console.log(typeof cInNumber);
+// console.log(cInNumber);
+// console.log(typeof dInNumber);
+// console.log(dInNumber);
+
+
+
+// let isLoggedIn = 1, isNotLoggedIn = 0;
+// let a = "";
+// let b = "abc";
+// let c = null;
+// let d = undefined;
+
+// let isLoggedInBoolean = Boolean(isLoggedIn);
+// let isNotLoggedInBoolean = Boolean(isNotLoggedIn);
+// let aBoolean = Boolean(a);
+// let bBoolean = Boolean(b);
+// let cBoolean = Boolean(c);
+// let dBoolean = Boolean(d);
+
+// console.log(typeof isLoggedInBoolean);
+// console.log(isLoggedInBoolean);
+// console.log(typeof isNotLoggedInBoolean);
+// console.log(isNotLoggedInBoolean);
+// console.log(typeof aBoolean);
+// console.log(aBoolean);
+// console.log(typeof bBoolean);
+// console.log(bBoolean);
+// console.log(typeof cBoolean);
+// console.log(cBoolean);
+// console.log(typeof dBoolean);
+// console.log(dBoolean);
+
+
+
+
+
+// ****************************Operations**************************** //
+let a = 102;
+// let aNegative = -a;         // Convert into negative value
+// console.log(aNegative);
+
+/*
+
+Addition => +
+Subtraction => -
+Multiplication => *
+Division => /
+Power => **
+Remainder => %
+
+*/
+
+
+// let greet = "Hello";
+// let name = " Puneet";
+// console.log(greet + name);
+
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log(1 + "2");
+// console.log(1 + + "2");
+// console.log(+ "1" + 2);
+// console.log(+ "1");
+// console.log(typeof(+ "1"));
+// console.log(+ 2 + "1");
+// console.log(typeof(+ 2 + "1"));
+// console.log(+ 2);
+// console.log(typeof(+ 2));
+
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + 2 + + "2");
