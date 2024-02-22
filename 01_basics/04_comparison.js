de5c1bd35e6036c2f6dfc2a350e5236d8fef0d01
Puneet <94126652+puneetyadav09@users.noreply.github.com>
2024-02-22 11:37:55 +0000

    update 04_comparison.js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);   /// Compares only value not checking datatype
+
+// console.log("1" == 1);    /// Compares only value not the datatype
+// console.log("1" === 1);   /// Compares both value and datatypes
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
