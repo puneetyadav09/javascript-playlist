788f65d681be80e30b27d48036a1efd6d62ab0b3
Puneet <94126652+puneetyadav09@users.noreply.github.com>
2024-02-24 17:25:35 +0000

    commit 06_num_and_maths.js

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,33 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.456;
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(3));
+
+const crore = 10000000;
+// console.log(crore.toLocaleString());
+// console.log(crore.toLocaleString("en-IN"));
+
+
+// ******************************************MATHS****************************************** //
+// console.log(Math);
+
+// console.log(Math.abs("-4"));
+// console.log(Math.round(4.53));
+
+// console.log(Math.ceil(4.53));
+// console.log(Math.floor(4.53));
+
+// console.log(Math.random());    ///Values between 0 and 1
+// console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
