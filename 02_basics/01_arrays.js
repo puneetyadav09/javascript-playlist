f1870fb895d3530e17d66030217a467d0c076d6d
Puneet <94126652+puneetyadav09@users.noreply.github.com>
2024-02-24 20:46:37 +0000

    commit 01_arrays.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,54 @@
+// arrays
+
+const names = ["Puneet", "Tushar", "Animesh"];
+const ages = [20, 23, 21];
+
+const newArr = new Array(1, 2, 3, 4);
+
+// console.log(names);
+// console.log(names[0]);
+// console.log(newArr[0]);
+
+// ************************Array Methods************************ //
+newArr.push(5);
+// console.log(newArr);
+newArr.push(6);
+// console.log(newArr);
+// newArr.pop();
+// console.log(newArr);
+
+// console.log(newArr);
+// newArr.unshift(0);
+// console.log(newArr);
+
+
+// console.log(newArr);
+// newArr.shift(0);
+// console.log(newArr);
+
+// console.log(newArr.includes(10));
+// console.log(newArr.indexOf(2));
+
+// const abc = newArr.join("-");
+// console.log(abc);
+// console.log(typeof abc);
+
+
+
+
+// slice
+// console.log(newArr);
+
+// const myarr = newArr.slice(1, 3);   // Returns new array and last digit i.e., 3 is not included
+// console.log(myarr);
+// console.log(newArr);
+
+
+
+
+// splice
+// console.log(newArr);
+
+// const myarr = newArr.splice(1, 3);   // Delete array from original array and last digit i.e., 3 is included
+// console.log(myarr);
+// console.log(newArr);
