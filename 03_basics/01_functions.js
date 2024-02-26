616c0b6becf73b79f8182bd6f2e29271dafff50a
Puneet <94126652+puneetyadav09@users.noreply.github.com>
2024-02-26 19:58:00 +0000

    commit 01_functions.js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+function myName(){
+    console.log("P");
+    console.log("u");
+    console.log("n");
+    console.log("e");
+    console.log("e");
+    console.log("t");
+}
+
+// myName();
+
+// function addNum(a, b){
+//     console.log(a + b);
+// }
+function addNum(a, b){
+    return (a + b);
+}
+
+const result = addNum(3, 5);
+console.log(`Result: ${result}`);
