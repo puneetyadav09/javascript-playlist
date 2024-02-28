eb5ea79d98ab9c324ea216cd67583fdff8002e00
Puneet <94126652+puneetyadav09@users.noreply.github.com>
2024-02-28 13:04:43 +0000

    commit 02_while_doWhile_loop.js

---
+++ 05_iterations/02_while_doWhile_loop.js
@@ -0,0 +1,33 @@
+// While Loop
+let index = 1;
+// while(index <= 10){
+//     console.log(index);
+//     index += 2;
+// }
+
+
+let myArray = ["flash", "batman", "superman"]
+let arr = 0;
+// while(arr < myArray.length){
+//     console.log(myArray[arr]);
+//     arr++;
+// }
+
+
+
+// do - while loop
+
+// let cases = 1;
+// do {
+//     console.log(cases);
+//     cases++;
+// } while (cases <= 10);
+
+
+// let score = 10;
+// do {
+//     console.log(score);
+//     score++;
+// } while (score < 10);
+
+
