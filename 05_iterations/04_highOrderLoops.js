5adf9871ebb3d51bbda1ec49d438186a85863af8
Puneet <94126652+puneetyadav09@users.noreply.github.com>
2024-02-28 13:29:50 +0000

    commit 04_highOrderLoops.js

---
+++ 05_iterations/04_highOrderLoops.js
@@ -0,0 +1,29 @@
+// For in loop
+
+const myObj = {
+    js: "Javascript",
+    cpp: "C plus plus",
+    rb: "Ruby",
+    py: "Python"
+}
+
+for (const key in myObj) {
+    // console.log(`${key} has value: ${myObj[key]}`);
+}
+
+const arr = ["js", "rb", "html", "py", "css"]
+for (const key in arr) {
+    // console.log(`${key} has value: ${arr[key]}`);
+}
+
+// const map = new Map();
+// map.set("IN", "INDIA");
+// map.set("USA", "UNITED STATE OF AMERICA");
+// map.set("FR", "FRANCE");
+// map.set("IN", "INDIA");
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+
