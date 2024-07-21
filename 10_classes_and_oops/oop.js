4d49d67cf10485744c5106553a0141e168f6f8c0
Puneet <94126652+puneetyadav09@users.noreply.github.com>
2024-07-21 14:53:11 +0000

    commit oop.js

--- 10_classes_and_oops/oop.js
+++ 10_classes_and_oops/oop.js
@@ -4,9 +4,10 @@ const user = {
     isSignedIn: true,
 
     getUserDetails: function(){
-        console.log(`Username: ${this.username}, Login Count: ${loginCount}, Is Signed in: ${isSignedIn}`);
+        console.log(`Username: ${this.username}, Login Count: ${this.loginCount}, Is Signed in: ${this.isSignedIn}`);
     }
 }
+
 
 console.log(user.username);
 console.log(user.getUserDetails());
